4240df38e07414e33e6de71b29e0154083ef3071
a.savchenko <anatoliy.savchenko@igt.com>
2025-06-30 23:17:49 +0200

    Video Recording added

---
+++ tests/VideoCaptureFromConfigTest.spec.js
@@ -0,0 +1,21 @@
+const { test, expect } = require('@playwright/test');
+
+
+//For this feature it's necessary to turn ON video on playwright.config.js as showed below
+/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer 
+    trace: 'on-first-retry',
+    screenshot:'on', 
+    video: "on" // off on only-on-failure on-first-retry retry-with-video
+
+/*It will allow us to capture the screenshot at the end of each test automatically*/
+
+test('Video recording tests', async ({page})=>{
+
+  await page.goto('https://demoblaze.com/');
+
+    //Login
+  await page.click('id=login2');
+  await page.fill('#loginusername', 'anatoliy58');
+  await page.fill('#loginpassword', 'strongpass123');
+  await page.click("//button[normalize-space()='Log in']");
+} )
