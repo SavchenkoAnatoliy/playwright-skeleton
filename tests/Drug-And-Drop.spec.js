e1e3590225a032d45af8983e45b6c07ef6674cab
a.savchenko <anatoliy.savchenko@igt.com>
2025-06-30 18:33:04 +0200

    Drug and Drop

---
+++ tests/Drug-And-Drop.spec.js
@@ -0,0 +1,23 @@
+import { test, expect } from '@playwright/test';
+
+test('Date Picker', async ({ page }) => {
+  //Open URL
+  await page.goto('http://testautomationpractice.blogspot.com/');
+ 
+  const elmenetToMove = await page.locator('#draggable')
+  const whereToDrop = await page.locator('#droppable')
+
+  //Perform Drug And Drop
+  //Approach 1
+  /*await elmenetToMove.hover();
+  await page.mouse.down();
+
+  await whereToDrop.hover();
+  await page.mouse.up();*/
+
+  //Approach 2
+  await elmenetToMove.dragTo(whereToDrop);
+
+  await page.waitForTimeout(5000)
+
+});
