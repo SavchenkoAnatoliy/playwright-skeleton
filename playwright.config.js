cacc98fadc417e4f02ac8a5dd2a7813d53b35ea3
Anatoliy S <exibi6058@gmail.com>
2025-07-08 23:15:06 +0200

    video records on

--- playwright.config.js
+++ playwright.config.js
@@ -41,7 +41,7 @@ export default defineConfig({
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'retain-on-failure',
     //screenshot:'on',
-    //video: "on"
+    video: "on"
   },
   /*timeout:5000,*/
 
